b2e7e69c4cb548d4e87667dd8bc5c78a86d52444
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2018-12-20 16:40:49 -0500

    buscador de json

---
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html',
+})
+export class HelloIonicPage {
+  jobs: any[]=[];
+  searchjobs: any[]=[];
+
+  constructor(private alertCtrl: AlertController) {
+    this.jobs.push({name: "barbie",precio: "4.5",venta: "6"});
+    this.jobs.push({name: "cami",precio: "4.5",venta: "6"});
+    this.jobs.push({name: "arbi",precio: "4.5",venta: "6"});
+    this.jobs.push({name: "tran",precio: "4.5",venta: "6"});
+    this.jobs.push({name: "mune",precio: "4.5",venta: "6"});
+    this.searchjobs = this.jobs;
+    console.log(this.searchjobs);
+  } 
+
+  onChangeTime() {
+    console.log("cambie")
+  }
+  
+  onInputTime(val:string) {
+    //console.log(val)
+    this.searchjobs = [];
+    for(var i = 0;i<this.jobs.length;i++) { 
+      if(this.jobs[i].descrip.search(val)>-1){
+        this.searchjobs.push(this.jobs[i]);
+      }
+    }
+  }
+  buttonClick(val:any){
+    console.log(val);
+    let texto = val.name+"<br>Precio: "+val.venta+"<br>Minimo: "+val.precio;
+    let alert = this.alertCtrl.create({
+      title: "resultado",
+      subTitle: texto,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  
+}
